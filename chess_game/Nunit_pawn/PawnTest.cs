de63ef2559e54b7763ff480274d1ca0f6505996e
joal22za <joal22za@student.ju.se>
2025-04-15 20:57:44 +0200

    Added some tests for movement of pawn and also added the implementation for capturing enemy pieces diagonally in the pawn class

---
+++ chess_game/Nunit_pawn/PawnTest.cs
@@ -0,0 +1,15 @@
+namespace Nunit_pawn
+{
+    public class PawnTest
+    {
+
+        [Test]
+        public void Moving_the_pawn()
+        {
+            // ARRANGE
+            
+            // ACT
+            // ASSERT
+        }
+    }
+}
