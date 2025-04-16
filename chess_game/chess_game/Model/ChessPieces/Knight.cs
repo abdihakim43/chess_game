00886bd2490388cf4fc93b6a1677d7dc5b2221e7
Abdihakim Mahamed <abdihakim.mahamed34@gmail.com>
2025-04-16 19:10:22 +0200

    added queen and knight to the starting layout

---
+++ chess_game/chess_game/Model/ChessPieces/Knight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_game.Model.ChessPieces
+{
+    public class Knight : ChessPiece
+    {
+        public Knight(string color, Position position) : base (color, position) { }
+
+        public override bool IsMoveValid(Position newPosition, ChessBoard board)
+        {
+            return true; // logic for movement later
+        }
+    }
+}
