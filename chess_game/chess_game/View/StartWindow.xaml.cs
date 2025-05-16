f2dfda40e4fd46904a2500c099555833892a3e08
Abdihakim Mahamed <abdihakim.mahamed34@gmail.com>
2025-05-16 22:56:39 +0200

    added start screen with PvP and PvE mode selection and added super basic bot logic

---
+++ chess_game/chess_game/View/StartWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace chess_game.View
+{
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PvP_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow gameWindow = new MainWindow("PvP");
+            gameWindow.Show();
+            this.Close();
+        }
+
+        private void PvE_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow gameWindow = new MainWindow("PvE");
+            gameWindow.Show();
+            this.Close();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
